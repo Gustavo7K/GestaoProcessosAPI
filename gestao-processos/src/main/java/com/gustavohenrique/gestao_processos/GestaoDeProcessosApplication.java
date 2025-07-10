be15daf0de639de20ca338c56f13f6fa6d21a017
Gustavo <gustavosales0704@gmail.com>
2025-07-09 22:16:56 -0300

    :tada: Commit inicial

---
+++ gestao-processos/src/main/java/com/gustavohenrique/gestao_processos/GestaoDeProcessosApplication.java
@@ -0,0 +1,13 @@
+package com.gustavohenrique.gestao_processos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeProcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeProcessosApplication.class, args);
+	}
+
+}
