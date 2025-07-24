ed536ab829a95b8e4a2f71055ce83cfe057f6027
Gustavo <gustavosales0704@gmail.com>
2025-07-24 20:45:35 -0300

    :sparkles: Adicionando método ao Subprocesso repository

--- gestao-processos/src/main/java/com/gustavohenrique/gestao_processos/repository/SubprocessoRepository.java
+++ gestao-processos/src/main/java/com/gustavohenrique/gestao_processos/repository/SubprocessoRepository.java
@@ -3,7 +3,9 @@ package com.gustavohenrique.gestao_processos.repository;
 import com.gustavohenrique.gestao_processos.entity.SubProcesso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SubprocessoRepository extends JpaRepository<SubProcesso, UUID> {
+    List<SubProcesso>findByProcessoId(UUID processoid);
 }
