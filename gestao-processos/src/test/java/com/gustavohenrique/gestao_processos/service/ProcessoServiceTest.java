7c29f70365090766d72d1badee4da93f94c91c65
Gustavo <gustavosales0704@gmail.com>
2025-07-30 02:30:34 -0300

    chore: renomeando antigo ProcessoTest

--- gestao-processos/src/test/java/com/gustavohenrique/gestao_processos/service/ProcessoServiceTest.java
+++ gestao-processos/src/test/java/com/gustavohenrique/gestao_processos/service/ProcessoServiceTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProcessoTest {
+public class ProcessoServiceTest {
     @Mock
     private QuadroRepository quadroRepository;
 
